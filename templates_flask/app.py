5a2e0a0186397ecceebdf8789dcd9bf1937f229d
claudiaastefanelli <stefanelli.claudia@iisgalvanimi.edu.it>
2023-04-14 09:02:24 +0000

    parte4

--- templates_flask/app.py
+++ templates_flask/app.py
@@ -5,6 +5,9 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    return render_template('hello.html', name='Claudia')
+@app.route('/units')
+def unitList():
+    mycursor.execute("SELECT * FROM Clash_Unit")
+    myresult= mycursor.fetchall()
+    return render_template('clash_units.html', units=myresult)
+    
